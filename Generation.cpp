4c97cc6b58b723b913bbcad8ab335c4428f1aadd
ThomasLYET <thomas.lyet@u-psud.fr>
2013-10-15 23:20:06 +0200

    Base Generation

---
+++ Generation.cpp
@@ -0,0 +1,11 @@
+#include "Generation.hpp"
+
+Generation::Generation
+()
+{
+}
+
+Generation::~Generation
+()
+{
+}
---
+++ Generation.hpp
@@ -0,0 +1,27 @@
+#ifndef GENERATION_HPP
+#define GENERATION_HPP
+
+template< typename G >
+class Generation
+{
+private:
+	//Condition c;
+	int nbIndividuParPopulation;
+	// Ici il nous faut un Set d'un couple (f(x),x) trié sur f(x) que l'on nomme G pour le moment
+	typename G population;
+
+public:
+	Generation();
+	~Generation();
+	
+	// Laquelle ?
+	Generation passerNouvelleGeneration();
+	// change a génération actuelle en la nouvelle génération
+	void passerNouvelleGeneration2();
+	G fusionPopulation ( G pop1, G pop2 );	
+	bool condArret(); 
+
+};
+
+
+#endif
